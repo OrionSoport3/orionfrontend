3ab86aaae94f40de43e06e6ae7119a1baae7d621
eddca <edd.cazj0420@gmail.com>
2024-06-14 17:59:40 -0600

    register con token

--- src/components/Subcomponents/InputLogin.tsx
+++ src/components/Subcomponents/InputLogin.tsx
@@ -9,14 +9,16 @@ type Props = {
   inside: string ;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void; 
   value?: string | number;
+  css?: string,
+  csslabel?: string,
 }
 
-export const InputLogin = ({label, name, id, placeholder, error, type, inside, onChange, value}: Props) => {
+export const InputLogin = ({label, name, id, placeholder, error, type, inside, onChange, value, css, csslabel}: Props) => {
 
   return (
-    <div className="w-full  flex flex-col px-5 md:px-2 relative pt-2 md:mt-2">
+    <div className={` ${css ?? 'w-full  flex flex-col px-5 md:px-2 relative pt-2 md:mt-2'}`}>
       <label htmlFor={label}
-      className="w-full text-white font-josefin text-xl nose:text-base md:text-xl py-2 md:py-3 pl-2 nose:mt-0 mt-2 truncate">
+      className={`${csslabel ?? 'w-full text-white font-josefin text-xl nose:text-base md:text-xl py-2 md:py-3 pl-2 nose:mt-0 mt-2 truncate'}`}>
         {inside}
       </label>
       <input value={value} onChange={onChange} type={type ?? 'text'} name={name} id={id} className="bg-white md:smartwatch:w-[200px] w-auto md:w-full rounded-full md:rounded-lg h-8 md:h-9 outline-none shadow-lg px-3" placeholder={placeholder}/>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -5,22 +5,26 @@ import { SignBoton } from "../components/Subcomponents/SignBoton";
 import { TituloSection } from "../components/register/TituloSection";
 import { Api } from "../services/Api";
 import * as Yup from 'yup';
-
-
 const Register = () => {
 
   const validationSchema = Yup.object({
     name: Yup.string().min(3, "El nombre minimo debe contener al menos 3 letras").max(100).required("Por favor ingrese un nombre"),
     email: Yup.string().email("Por favor ingrese un correo valido").required(),
+    birthday: Yup.date().required(),
     password: Yup.string().min(8, "La contraseña debe contener al menos 8 caracteres").max(20,"Su contraseña debe estar entre 8 y 20 caracteres").required("Por favor ingrese una contraseña"),
-    password_confirmation: Yup.string().oneOf([Yup.ref("password")], "Las contraseñas no coinciden").required("Confirme su contraseña")
+    password_confirmation: Yup.string().oneOf([Yup.ref("password")], "Las contraseñas no coinciden").required("Confirme su contraseña"),
+    token: Yup.string().ensure()
   });
 
   const valoresIniciales = {
     name: '',
+    lastname: '',
+    birthday: '',
+    departamento: '',
     email: '',
     password: '',
     password_confirmation: '',
+    token: '',
   };
 
   const onSubmit = (values: typeof valoresIniciales) => {
@@ -64,7 +68,11 @@ const Register = () => {
                 <InputLogin type="email" name="email" label="correo" inside="Escriba su correo:" onChange={handleChange} error={errors.email} value={values.email}/>
               </div>
                 <InputLogin type="password" name="password" label="password" placeholder="****************" inside="Ingrese su contraseña:" error={errors.password} onChange={handleChange} value={values.password}/>
-                <InputLogin type="password" name="password_confirmation" label="password_confirmation" placeholder="****************" inside="Confirme su contraseña:" error={errors.password_confirmation} onChange={handleChange}value={values.password_confirmation} />
+                <InputLogin type="password" name="password_confirmation" label="password_confirmation" placeholder="****************" inside="Confirme su contraseña:" error={errors.password_confirmation} onChange={handleChange} value={values.password_confirmation} />
+                <div className="w-4/5 mt-10 h-[125px] BP1:h-auto bg-blue-950 flex justify-center items-center relative z-50 rounded-3xl px-4 BP1:px-10">
+                  <InputLogin csslabel="w-full pl-1 text-white font-josefin text-base nose:text-base md:text-xl mt-0 py-0 pb-3 truncate" css="flex flex-col justify-center w-full items-cent pt-0 h-[150px]"
+                  type="password" name="token" label ="token" placeholder="****" inside="Token de seguridad" error={errors.token} value={values.token} onChange={handleChange}/>
+                </div>
               <SignBoton inside="Ingresar" type="submit" /> 
             </form>
           )}
