1a3969123fe44f86688c0f04723a3abdf6f718a9
eddca <edd.cazj0420@gmail.com>
2024-06-29 13:56:02 -0600

    estado para los objetos seleccionables

--- src/pages/NewServiceForm.tsx
+++ src/pages/NewServiceForm.tsx
@@ -18,21 +18,20 @@ export const NewServiceForm = () => {
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
   const [users, setUsers] = useState<any[]>([]);
   const token = useSelector((state: RootState) => state.auth.token);
-  const [isSelect, setSelect] = useState(false);
+  const usuariosState = useSelector((state: RootState) => state.select.items)
   const dispatch = useAppDispatch();
 
-  const selectedItems = useSelector((state: RootState) => state.select.items.filter(item => item.isSelected));
-  const unselectedItems = useSelector((state: RootState) => state.select.items.filter(item => !item.isSelected));
+  const selectedItems = users.filter(user => user.isSelected);
+  const unselectedItems = users.filter(user => !user.isSelected);
 
   const manejarSelect = (field: string, value: string, setFieldValue: (field: any, value: string, shouldValidate?: boolean | undefined) => void) => {
     setSelectedOption(value);
     setFieldValue(field,value);
   };
 
+
   const selection = (id: number) => {
-    const updatedUsers = users.map((user) =>
-      user.id === id  ? { ...user, isSelected: !user.isSelected } : user
-    );
+    const updatedUsers =  users.map(user => user.id ===id ? {...user, isSelected: !user.isSelected} : user)
     dispatch(setItems(updatedUsers));
     setUsers(updatedUsers);
   }
@@ -48,20 +47,18 @@ export const NewServiceForm = () => {
           id: user.id,
           name: user.nombre,
           departamento: user.departamento,
-          isSelected: false,
+          isSelected: usuariosState.find((storedUser: any) => storedUser.id === user.id)?.isSelected || false,
       }));
-      setUsers(usuarios);
-  } catch (error) {
-      console.error('Error fetching users:', error);
-      throw new Error('Failed to fetch users');
+     setUsers(usuarios);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        throw new Error('Failed to fetch users');
+    }
   }
-  }
-
     useEffect(() => {
       fetchUsers();
-      console.log(users)
+      console.log(users);
     }, [token]);
-
 
   const validationSchema = Yup.object({
     nombre_proyecto: Yup.string().required("Por favor ingrese el titulo del proyecto"),
@@ -82,12 +79,12 @@ export const NewServiceForm = () => {
   }
 
   return (
-    <div className="w-screen h-screen fixed text-black flex flex-col items-center bg-gradient-to-t from-gray-300 to-colores-pantalla-form px-12">
+    <div className="w-screen h-screen fixed text-black flex flex-col items-center bg-gradient-to-t from-gray-300 to-colores-pantalla-form px-7">
       <Navbar/>
       <Toaster/>
       <div className="flex w-full h-full">
-        <div className="flex w-full items-center justify-center mt-4">
-          <div className="w-full h-full ">
+        <div className="flex w-full items-center justify-center mt-4 ">
+          <div className="w-[70%] h-[95%] overflow-y-auto pb-10 pr-4">
             <Formik
               initialValues={valoresIniciales}
               onSubmit={onSubmit}
@@ -100,7 +97,7 @@ export const NewServiceForm = () => {
               handleSubmit,
               setFieldValue,
             }) => (
-              <form onSubmit={handleSubmit} className="space-y-5 overflow-y-auto">
+              <form onSubmit={handleSubmit} className="space-y-5">
                 <NewTitulo texto="NUEVO SERVICIO" />
                 <InputNew value={values.nombre_proyecto} onChange={handleChange} error={errors.nombre_proyecto} texto="Titulo del proyecto" name="nombre_proyecto" type="text" incheck={false} />
                 <div className="flex flex-row w-full h-auto">
@@ -111,27 +108,28 @@ export const NewServiceForm = () => {
                 <NewTitulo texto="PERSONAL ENCARGADO"/>
                 {users.length > 0 ? (
                     <div className="w-full h-auto flex flex-col">
-                      <div className="w-full h-auto py-5 pr-8 flex flex-row">
-                          <h2 className="text-xl font-josefin">Personal:</h2>
-                          <div className="flex flex-wrap h-auto">
+                      <div className="w-full h-auto pb-6 pr-8 flex flex-row border-b-[1px] border-b-gray-400">
+                          <h2 className="text-xl font-josefin pt-1">Personal:</h2>
+                          <div className="flex flex-wrap h-auto px-3 space-x-3">
                             {selectedItems.map((item) => (
                               <>    
-                              <h1>{`usuario: ${item}`}</h1>                          
-                              <ItemSeleccionable key={item.name} name={item.name} selected={() => selection(item.id)} isSelected={isSelect} />
+                              <ItemSeleccionable key={item.id} name={item.name} selected={() => selection(item.id)} isSelected={item.isSelected} />
                               </>
                             ))}
                           </div>
                       </div>
-                          <h2>Unselected Items</h2>
-                          <div>
-                            {unselectedItems.map((item) => (
-                              <>
-                              <ItemSeleccionable key={item.name} name={item.name} selected={() => selection(item.id)} isSelected={isSelect} />                              
-                              </>
-                            ))}
+                      <div className="w-full h-auto space-y-2 pt-5">
+                        {unselectedItems.map((item, index) => (
+                          <div key={item.id} className="w-full h-auto pl-4">
+                             {index === 0 || item.departamento !== unselectedItems[index - 1].departamento ? (
+                              <h2 className="font-josefin py-3">{item.departamento}</h2>
+                            ) : null}
+                            <ItemSeleccionable key={item.id} name={item.name} selected={() => selection(item.id)} isSelected={item.isSelected}/>
                           </div>
+                        ))}
+                      </div>
                           {/* <h2>Fetched usuarios: {`usuarios: ${JSON.stringify(users)}`}</h2> */}
-                      </div>
+                    </div>
                   ) : (
                     <>
                     <div>Hubo un error al cargar los usuarios</div>
@@ -142,7 +140,7 @@ export const NewServiceForm = () => {
             )}
             </Formik>
           </div>
-          <div className="w-full h-full">
+          <div className="w-[30%] h-full">
           </div>
         </div>
       </div>
